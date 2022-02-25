04a2557206b8b9a30c0188898c4bfced04c39e8a
Cas Cloots <cloots.cas@gmail.com>
2022-02-25 14:25:12 +0100

    project setup

---
+++ src/components/case.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Slider from ".../component/slider";
+
+const Case = () => {
+  return (
+  <section>
+    <div class="case-header">
+      <h2 class="case-category">↓ Web Design ↓</h2>
+      <h1 class="case-title"><span class="case-client">Microsoft</span> - <span>Get to know Azure, Turn your ideas into innovation with trusted Azure products and services</span></h1>
+    </div>
+    <Slider />
+    <div>
+      
+    </div>
+    </section>
+  )
+}
+
+export default Case
---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <section class="hero">
+      <div class="content">
+        <h2 class="title">Hi, there</h2>
+        <h1 class="intro">
+          Cas Cloots is a designer working across digital and graphic design
+          with a passion for photography and motion<br />
+          Available for hire <a href="mailto:cloots.cas@gmail.com">⟶ cloots.cas@gmail.com</a>
+        </h1>
+      </div>
+    </section>
+  )
+}
+
+export default Header
