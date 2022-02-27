a01ad285ae8a3e142dbc75965174d857715037b8
Cas Cloots <cloots.cas@gmail.com>
2022-02-27 12:54:12 +0100

    Cleanup

---
+++ .cache/sync-requires.js
@@ -0,0 +1,10 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/ccloots/Documents/GitHub/cascloots.com/.cache/dev-404-page.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/ccloots/Documents/GitHub/cascloots.com/src/pages/index.js"))
+}
+
